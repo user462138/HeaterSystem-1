d26dac1c3d365e9a6ae5440860e77efbe2153843
janvandenpoel <jan.vandenpoel@ap.be>
2024-07-18 12:39:08 +0200

    Commit 2.6 : Test 2 – WorkWhenTemperatureLessThanLowerBoundaryEnableHeatingElement

--- HeaterSystem.UnitTests/ThermostatTests.cs
+++ HeaterSystem.UnitTests/ThermostatTests.cs
@@ -31,4 +31,30 @@ public class ThermostatTests
         heatingElementMock.Verify(x => x.Enable(), Times.Never);
         heatingElementMock.Verify(x => x.Disable(), Times.Never);
     }
+    [TestMethod]
+    public void WorkWhenTemperatureLessThanLowerBoundaryEnableHeatingElement()
+    {
+        // --- Arrange ---
+        // Mock the objects used by the Thermostat object
+        Mock<ITemperatureSensor> temperatureSensorMock = new Mock<ITemperatureSensor>();
+        Mock<IHeatingElement> heatingElementMock = new Mock<IHeatingElement>();
+
+        // Create the test object
+        Thermostat thermostat = new Thermostat(temperatureSensorMock.Object, heatingElementMock.Object);
+
+        // Set the setpoint and offset
+        thermostat.Setpoint = 20.0;
+        thermostat.Offset = 2.0;
+
+        // Configure the mock object to get the temperature less than lower boundary = 17.0
+        temperatureSensorMock.Setup(x => x.GetTemperature()).Returns(17.0);
+
+        // --- Act ---
+        thermostat.Work();
+
+        // --- Assert ---
+        // Verify that neither the method Enable nor the method Disable of the heatingElementMock object is called (= Do Nothing)
+        heatingElementMock.Verify(x => x.Enable(), Times.Once);
+        heatingElementMock.Verify(x => x.Disable(), Times.Never);
+    }
 }
