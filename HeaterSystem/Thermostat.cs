47b4fae1eddfeba73bcd7f35abddbc090a8d4452
janvandenpoel <jan.vandenpoel@ap.be>
2024-07-18 11:51:43 +0200

    Commit 2.4a : Test 0 – Add properties Setpoint and Offset

--- HeaterSystem/Thermostat.cs
+++ HeaterSystem/Thermostat.cs
@@ -11,6 +11,20 @@ public class Thermostat
     private readonly ITemperatureSensor temperatureSensor;
     private readonly IHeatingElement heatingElement;
 
+    private double setpoint;
+    public double Setpoint
+    {
+        get { return setpoint; }
+        set { setpoint = value; }
+    }
+
+    private double offset;
+    public double Offset
+    {
+        get { return offset; }
+        set { offset = value; }
+    }
+
     public Thermostat(ITemperatureSensor temperatureSensor, IHeatingElement heatingElement)
     {
         this.temperatureSensor = temperatureSensor;
