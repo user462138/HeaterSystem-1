ec671dd71ff94929b1493af37cde3d58f94dc9aa
janvandenpoel <jan.vandenpoel@ap.be>
2024-07-18 10:44:57 +0200

    Commit 2.4 : Test 0 - Application structure

--- HeaterSystem/Program.cs
+++ HeaterSystem/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HeaterSystem;
+
+ITemperatureSensor temperatureSensor = new TemperatureSensor();
+IHeatingElement heatingElement = new HeatingElement();
+
+Thermostat thermostat = new Thermostat(temperatureSensor, heatingElement);
+
+while (true)
+{
+    thermostat.Work();
+    Thread.Sleep(5000);
+}
---
+++ HeaterSystem/Thermostat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterSystem;
+
+public class Thermostat
+{
+    private readonly ITemperatureSensor temperatureSensor;
+    private readonly IHeatingElement heatingElement;
+
+    public Thermostat(ITemperatureSensor temperatureSensor, IHeatingElement heatingElement)
+    {
+        this.temperatureSensor = temperatureSensor;
+        this.heatingElement = heatingElement;
+    }
+
+    public void Work()
+    {
+        throw new NotImplementedException();
+    }
+}
