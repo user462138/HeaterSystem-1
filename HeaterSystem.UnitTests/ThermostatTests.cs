0b12f71819b099f844cabf8a68c369677cfa7dc0
janvandenpoel <jan.vandenpoel@ap.be>
2024-07-19 10:42:59 +0200

    Commit 2.6b : Refactoring Tests

--- HeaterSystem.UnitTests/ThermostatTests.cs
+++ HeaterSystem.UnitTests/ThermostatTests.cs
@@ -5,21 +5,29 @@ namespace HeaterSystem.UnitTests;
 [TestClass]
 public class ThermostatTests
 {
+    private Mock<ITemperatureSensor> temperatureSensorMock = null;
+    private Mock<IHeatingElement> heatingElementMock = null;
+
+    private Thermostat thermostat;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        // Mock the objects used by the thermostat object
+        temperatureSensorMock = new Mock<ITemperatureSensor>();
+        heatingElementMock = new Mock<IHeatingElement>();
+
+        // Create the test object, set the setpoint and offset
+        thermostat = new Thermostat(temperatureSensorMock.Object, heatingElementMock.Object)
+        {
+            Setpoint = 20.0,
+            Offset = 2.0
+        };
+    }
     [TestMethod]
     public void WorkWhenTemperatureBetweenBoundariesDoNothing()
     {
         // --- Arrange ---
-        // Mock the objects used by the Thermostat object
-        Mock<ITemperatureSensor> temperatureSensorMock = new Mock<ITemperatureSensor>();
-        Mock<IHeatingElement> heatingElementMock = new Mock<IHeatingElement>();
-
-        // Create the test object
-        Thermostat thermostat = new Thermostat(temperatureSensorMock.Object, heatingElementMock.Object);
-
-        // Set the setpoint and offset
-        thermostat.Setpoint = 20.0;
-        thermostat.Offset = 2.0;
-
         // Configure the mock object to get the temperature between boundaries = 19.0
         temperatureSensorMock.Setup(x => x.GetTemperature()).Returns(19.0);
 
@@ -35,17 +43,6 @@ public class ThermostatTests
     public void WorkWhenTemperatureLessThanLowerBoundaryEnableHeatingElement()
     {
         // --- Arrange ---
-        // Mock the objects used by the Thermostat object
-        Mock<ITemperatureSensor> temperatureSensorMock = new Mock<ITemperatureSensor>();
-        Mock<IHeatingElement> heatingElementMock = new Mock<IHeatingElement>();
-
-        // Create the test object
-        Thermostat thermostat = new Thermostat(temperatureSensorMock.Object, heatingElementMock.Object);
-
-        // Set the setpoint and offset
-        thermostat.Setpoint = 20.0;
-        thermostat.Offset = 2.0;
-
         // Configure the mock object to get the temperature less than lower boundary = 17.0
         temperatureSensorMock.Setup(x => x.GetTemperature()).Returns(17.0);
 
